9ddee12ffa96762d5bba446372b7f29c26d373c5
Thomas Salah <thomas.salah@gmail.com>
2020-03-02 09:44:09 +0000

    building the tweeter clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
+import {handleInitialData } from '../actions/shared'
+import Dashboard  from './Dashboard'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Nav from './Nav'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
+  componentDidMount () {
+    const { dispatch } = this.props
+    dispatch(handleInitialData())
+  }
+
   render() {
+    const { loading } = this.props
     return (
-      <div>
-        Starter Code
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className='container'>
+            <Nav />
+            { loading ?
+              null :
+              <div>
+              <Route path='/' exact component={Dashboard} />
+              <Route path='/tweet/:id' component={TweetPage} />
+              <Route path='/new' component={NewTweet} />
+            </div>
+            }
+          </div>
+        </Fragment>
+      </Router>
+
     )
   }
 }
+ function mapStateToProps ({authedUser}) {
+    return{
+      loading: authedUser === null
+    }
+ }
 
-export default App
+
+export default connect(mapStateToProps)(App)
